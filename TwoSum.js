6de1ade8eecfc16b423249a28c266f0582b7586e
shivaganesh.gajavelli <92672776+shivaganesh9515@users.noreply.github.com>
2025-06-09 11:02:15 +0530

    Update TwoSum.js

--- TwoSum.js
+++ TwoSum.js
@@ -1,9 +1,8 @@
 function twoSum(nums, target) {
-  let map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    let diff = target - nums[i];
-    if (map.has(diff)) return [map.get(diff), i];
-    map.set(nums[i], i);
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) return [i, j];
+    }
   }
 }
 // Example usage:
